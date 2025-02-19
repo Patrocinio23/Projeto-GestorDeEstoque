971b7f930c527c504e18abd88efd9f5286e4c3b9
Estevão Patrocínio <estevao.patrocinio@gmail.com>
2025-02-19 09:50:33 -0300

    add atualizações

--- Api/src/main/java/com/example/springboot/controllers/ProductController.java
+++ Api/src/main/java/com/example/springboot/controllers/ProductController.java
@@ -3,6 +3,7 @@ package com.example.springboot.controllers;
 import com.example.springboot.dtos.ProductRecordDto;
 import com.example.springboot.models.ProductModel;
 import com.example.springboot.repositories.ProductRepository;
+import com.example.springboot.service.ProductService;
 import com.example.springboot.services.TwilioService.TwilioService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
@@ -18,50 +19,37 @@ import java.util.Optional;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+
+//Essas anotações permitem que voce associe URLs específicas com métodos de manipulação HTTP no seu controlador, tornando a construção de APIs web mais simples.
+//uma anotação @ é simplesmente uma marca que voce coloca em cima de uma classe, método, campo, etc. Elas podem ser interpretadas por ferramentas, framework ou bibliotecas que usam essas informações.
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 	
 	@Autowired
-	ProductRepository productRepository;
+	ProductService productService;
         
         @Autowired
         private TwilioService twilioService;
         
 	
 	@GetMapping("/products")
-	//Essas anotações permitem que voce associe URLs específicas com métodos de manipulação HTTP no seu controlador, tornando a construção de APIs web mais simples.
-	//uma anotação @ é simplesmente uma marca que voce coloca em cima de uma classe, método, campo, etc. Elas podem ser interpretadas por ferramentas, framework ou bibliotecas que usam essas informações.
-	public ResponseEntity<List<ProductModel>> getAllProducts(){
-		List<ProductModel> productsList = productRepository.findAll();
-		//essa lista foi declarada para aceitar apenas objetos do tipo ProductModel ou de suas subclasses.
-		//os sinais <> são usados para classes Genéricas, onde o tipo de dado é definido no momento do instanciamento.
-		if(!productsList.isEmpty()) {
-			//productList é gerada e populada a partir de uma coleção de objetos do tipo ProductModel, que pode ser retornado de uma cosulta ao banco de dados ou criada manualmente
-			for(ProductModel product : productsList) {
-				//em cada iteração a variavel product será uma instancia individual de um objeto ProductModel.
-				//product é uma instancia da classe ProductModel
-				//productList é uma instancia de uma lista (como List<ProductModel>), que contem várias instancias (objetos) da classe ProductModel.
-				Integer id = product.getidProduct();
-				product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
-				//HATEOS é uma forma de enriquecer as respostas da API com links que indicam o que o cliente pode fazer a seguir, facilitando a interação sem precisar saber os detalhes das rotas.
-			}
-		}
-		return ResponseEntity.status(HttpStatus.OK).body(productsList);
-	}
-
-	@GetMapping("/products/{id}")
-	public ResponseEntity<Object> getOneProduct(@PathVariable Integer id){
-		Optional<ProductModel> product = productRepository.findById(id);
-		if(product.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
-                        
-		}
-                product.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
-		return ResponseEntity.status(HttpStatus.OK).body(product.get());
+	public ResponseEntity<List<ProductModel>> getAllProducts() {
+		List<ProductModel> productsList = productService.getAllProducts();
+	return ResponseEntity.status(HttpStatus.OK).body(productsList);
 	}
 	
-	@PostMapping("/products")
+   //essa lista foi declarada para aceitar apenas objetos do tipo ProductModel ou de suas subclasses.
+   //os sinais <> são usados para classes Genéricas, onde o tipo de dado é definido no momento do instanciamento.
+	
+	
+   @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable Integer id){
+		return productService.getOneProduct(id);
+	}
+	
+	/*@PostMapping("/products")
 	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
 		var productModel = new ProductModel();
 		BeanUtils.copyProperties(productRecordDto, productModel);
@@ -94,7 +82,7 @@ public class ProductController {
 		var productModel = productO.get();
 		BeanUtils.copyProperties(productRecordDto, productModel);
 		return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
-	}
+	}*/
 
     
 }
--- Api/src/main/java/com/example/springboot/service/ProductService.java
+++ Api/src/main/java/com/example/springboot/service/ProductService.java
@@ -18,17 +18,28 @@ public class ProductService {
 @Autowired
 ProductRepository productRepository;
 
-public List<ProductModel> getAllProducts() {
+public ResponseEntity<List<ProductModel>> getAllProducts() {
     List<ProductModel> productsList = productRepository.findAll();
-    if(!productsList.isEmpty()) {
-        for(ProductModel product : productsList) {
+    if(productsList.isEmpty()) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(productsList);
+     } else {
+           for(ProductModel product : productsList) {
             Integer id = product.getidProduct();
             product.add(linkTo(methodOn(ProductController.class).getOneProduct(id)).withSelfRel());
         }
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
-    return(productsList);
 }
+                              /*FOR*/
 
+//productList é gerada e populada a partir de uma coleção de objetos do tipo ProductModel que pode ser retornando de uma consulta ao banco de dados ou criada manualmente.
+//productList é uma lista de objetos do tipo ProductModel
+//em cada iteração a variavel product será uma instancia individual de um objeto ProductModel.
+//product é uma instancia da classe ProductModel
+//productList é uma instancia de uma lista (como List<ProductModel>), que contem várias instancias (objetos) da classe ProductModel.
+
+
+                            /*METHODON */
 
 /*Quando voce usa o "methodOn(ProductController.class)", voce esta passando ProductController.class diretamente para o methodOn(). Isso faz com que o methodOn() utilize a reflexão para "criar" a instancia virtual do controlador e chamar o método desejado sem a necessidade de voce instancia-lo manualmente. */
 
@@ -39,6 +50,7 @@ public List<ProductModel> getAllProducts() {
  
  * o método "getOneProduct(id)" será chamado virtualmente (não precisa instanciar um objeto de ProductController explicitamente).
  */ 
+
 
  public ResponseEntity<Object> getOneProduct(Integer id) {
     Optional<ProductModel> product = productRepository.findById(id);
