f43927ed5fad7005e10ef9d8616de5723c9d9e90
Estevão Patrocínio <estevao.patrocinio@gmail.com>
2025-07-07 10:13:18 -0300

    Update ProductService.java

--- Api/src/main/java/com/example/springboot/service/ProductService.java
+++ Api/src/main/java/com/example/springboot/service/ProductService.java
@@ -1,5 +1,16 @@
 package com.example.springboot.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.springboot.dtos.ProductRecordDto;
+import com.example.springboot.models.ProductModel;
+import com.example.springboot.repositories.ProductRepository;
+
 public class ProductService {
   
 }
